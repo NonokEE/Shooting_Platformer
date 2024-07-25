2e07ceb0e41f06ed09a52f3d6d8fc665948c886d
NonokEE <shshrdl@naver.com>
2024-07-25 22:01:10 +0900

    feat : Implement library "FSM"(Finite State Machine)

---
+++ Assets/03.Scripts/Library/FiniteStateMachine.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+/// <summary> </summary>
+/// <remarks>
+///
+/// </remarks>
+public class FiniteStateMachine<T_Sender, T_State> where T_Sender : MonoBehaviour where T_State : Enum
+{
+    /******* FIELD *******/
+    //~ Properties ~//
+    public State<T_Sender> CurrentState { get; private set; }
+    public enum EventType { Enter, Exit, Update }
+    public bool SetUpdate;
+
+    //~ Variables ~//
+    private T_Sender sender;
+    private Dictionary<T_State, State<T_Sender>> states;
+    private Dictionary<T_State, Dictionary<T_State, Action>> transitions;
+
+    /******* METHOD *******/
+    public FiniteStateMachine(T_Sender headInstance, T_State startState)
+    {
+        sender = headInstance;
+
+        states = new Dictionary<T_State, State<T_Sender>>();
+        foreach(T_State stateName in Enum.GetValues(typeof(T_State)))
+        {
+            states.Add(stateName, new State<T_Sender>(sender, stateName.ToString(), Convert.ToInt32(stateName)));
+        }
+        CurrentState = states[startState];
+
+        transitions = new Dictionary<T_State, Dictionary<T_State, Action>>();
+    }
+
+    public void AddEvent(T_State state, EventType eventType, Action action)
+    {
+        switch(eventType)
+        {
+            case EventType.Enter : states[state].OnEnter  += action; break;
+            case EventType.Exit  : states[state].OnExit   += action; break;
+            case EventType.Update: states[state].OnUpdate += action; break;
+        }
+    }
+
+    public void AddTransition(T_State depart, T_State dest, Action action)
+    {
+        if(Enum.Equals(depart, dest)) 
+        {
+            Debug.LogWarning(sender.name + "'s FSM of " + nameof(T_State) + " is trying to add transition about same state: " + depart);
+            return;
+        }
+        
+        if(!transitions.ContainsKey(depart))
+        {
+            transitions.Add(depart, new Dictionary<T_State, Action>());    
+        }
+        if(!transitions[depart].ContainsKey(dest))
+        {
+            transitions[depart].Add(dest, action);
+        } 
+    }
+
+    public void SetStatus(T_State next)
+    {
+        if(string.Equals(next.ToString(), CurrentState.Name))
+        {
+            Debug.LogWarning(sender.gameObject.name + " is trying to change to same status " + CurrentState);
+        }
+        else
+        {
+            T_State cur = (T_State) Enum.Parse(typeof(T_State), CurrentState.Name);
+
+            if(SetUpdate)
+            {
+                SetUpdate = false;
+            }
+            CurrentState.OnExit.Invoke();
+
+            if(transitions.ContainsKey(cur) && transitions[cur].ContainsKey(next)) 
+            {
+                transitions[cur][next].Invoke();
+            }
+
+            CurrentState = states[next];
+            CurrentState.OnEnter.Invoke();
+        }
+    }
+
+    /// <summary> This method should be contained in sender's MonoBehaviour.Update() if you want to make FSM executes it's Update function.  </summary>
+    public void Update()
+    {
+        if(SetUpdate) CurrentState.OnUpdate.Invoke();
+    }
+}
---
+++ Assets/03.Scripts/Library/State.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+/// <summary> </summary>
+/// <remarks>
+///
+/// </remarks>
+public class State<T_Sender> 
+{
+    //~ Properties ~//
+    public int StateValue { get; private set; }
+    public string Name { get; private set; }
+
+    public Action OnEnter = ()=>{};
+    public Action OnExit = ()=>{};
+    public Action OnUpdate = ()=>{};
+
+    //~ Variables ~//
+    private T_Sender sender;
+
+    //~ Methods ~//
+    public State(T_Sender senderInstance, string name, int stateValue)
+    {
+        sender = senderInstance;
+        Name = name;
+        StateValue = stateValue;
+    }
+
+    public void Test()
+    {
+
+    }
+}

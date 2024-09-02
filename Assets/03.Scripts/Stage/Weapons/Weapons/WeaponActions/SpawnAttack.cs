e63d8febdd6d4660f42f2e504fca0a85147b95cd
NonokEE <shshrdl@naver.com>
2024-09-03 02:36:20 +0900

    refactor : Apply DOTween for cooldown

--- Assets/03.Scripts/Stage/Weapons/Weapons/WeaponActions/SpawnAttack.cs
+++ Assets/03.Scripts/Stage/Weapons/Weapons/WeaponActions/SpawnAttack.cs
@@ -2,35 +2,38 @@ using System;
 using System.Collections;
 
 using UnityEngine;
+using DG.Tweening;
 
 namespace Stage
 {
-    public class SpawnAttack : MonoBehaviour, ILeftDown, ILeftHold
+    public class SpawnAttack : WeaponAction, ILeftDown, ILeftHold
     {
         //~~ Interface ~~//
         public float Cooltime;
         private bool isReady = true;
-
-        public Attack Prefab;
-        public AttackInfo Info;
-        public Action<Attack> SpawnCallback = (inst) => {};
+        private Tween cooldownTween;
 
         void ILeftDown.Invoke(){BasicSpawnAttack();}
         void ILeftHold.Invoke(){BasicSpawnAttack();}
 
         private void BasicSpawnAttack()
         {
-            if (Cooltime == 0 || isReady)
+            if(isReady)
             {
-                Attack inst = Instantiate(Prefab);
-                inst.AttackInfo = Info;
+                isReady = false;
+
+                Attack inst = Instantiate(AttackPrefab);
+                inst.AttackInfo = attackInfo;
                 SpawnCallback(inst);
-                if(Cooltime != 0) 
-                {
-                    isReady = false;
-                    StartCoroutine(Cooldown(Cooltime));
-                }
+                
+                float timer = Cooltime;
+                cooldownTween = DOTween.To(() => timer, x => timer = x, 0, Cooltime).OnComplete(() => { timer = Cooltime ; isReady = true;});
             }
+        }
+
+        private void OnDestroy() 
+        {
+            cooldownTween.Kill();
         }
 
         private IEnumerator Cooldown(float time)
